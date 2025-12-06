c080fac2879f590d293c40600f6a418afcd2268e
notaravinth <aravinthravichandran2004@gmail.com>
2025-12-06 09:00:05 +0530

    react-practice

---
+++ practicing-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='main-container'>
+      <h1 className='heading'>Hello World!</h1>
+      <p className='paragraph'>Welcome to React</p>
+    </div>
+  )
+}
+
+export default App
