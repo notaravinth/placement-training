3cca00ae736b5968bae3a7fdee9c8b6093403dc0
notaravinth <aravinthravichandran2004@gmail.com>
2025-12-13 22:52:54 +0530

    java-practice

--- java-practice/HelloWorld.java
+++ java-practice/HelloWorld.java
@@ -1,8 +1,5 @@
-public class HelloWorld{
+public class HelloWorld {
     public static void main(String[] args) {
-        String s1 = "abc";
-        String s2 = "abc";
-        String s3 = new String(original:"abc");
-        System.out.println(s1.equalsIgnoreCase(s2));
+      
     }
 }
